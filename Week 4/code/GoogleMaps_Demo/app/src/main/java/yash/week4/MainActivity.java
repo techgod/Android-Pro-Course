b290c6cace3b658651d550175f517a5e228b9149
Yash Pradhan <yash.pradhan@gmail.com>
2018-09-24 13:54:16 +0530

    add code

---
+++ Week 4/code/GoogleMaps_Demo/app/src/main/java/yash/week4/MainActivity.java
@@ -0,0 +1,39 @@
+package yash.week4;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MarkerOptions mo = new MarkerOptions();
+        //TODO: Replace with your home address.
+        LatLng lng = new LatLng(12.9344948, 77.5323277);
+        mo.position(lng);
+        googleMap.addMarker(mo);
+
+        //TODO: Bonus - Add a circle to the map!
+    }
+}
