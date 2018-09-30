934187b58dc295c8a2c40ccda0ae4c8b621ec6cc
Tech God <yash.pradhan@gmail.com>
2018-09-30 14:59:44 +0530

    Update SecondActivity.java

--- IOWorkshop/Week2/SecondActivity.java
+++ IOWorkshop/Week2/SecondActivity.java
@@ -1,13 +1,19 @@
-package yash.ioworkshop2;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+
+        String rating = intent.getStringExtra("rating");
+        String comments = intent.getStringExtra("comments");
+
+        TextView rating_tv = findViewById(R.id.rating2);
+        TextView comments_tv = findViewById(R.id.comments2);
+
+        rating_tv.setText("Rating:"+rating);
+        comments_tv.setText("Comments:"+comments);
     }
 }
